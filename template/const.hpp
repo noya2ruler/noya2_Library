72e4ddc3b91b8ade1c30c5ceba8db4261d1bc18c
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-07-17 19:54:12 +0900

    divide template

---
+++ template/const.hpp
@@ -0,0 +1,14 @@
+namespace noya2{
+
+const int iinf = 1'000'000'007;
+const long long linf = 2'000'000'000'000'000'000LL;
+const long long mod998 = 998244353;
+const long long mod107 = 1000000007;
+const long double pi = 3.14159265358979323;
+const vector<int> dx = {0,1,0,-1,1,1,-1,-1};
+const vector<int> dy = {1,0,-1,0,1,-1,-1,1};
+const string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alp = "abcdefghijklmnopqrstuvwxyz";
+const string NUM = "0123456789";
+
+} // namespace noya2
