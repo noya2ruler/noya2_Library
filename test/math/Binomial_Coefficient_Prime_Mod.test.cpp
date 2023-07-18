8f7da7d18def7e552da8d2a6cab10381c23c1a0f
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-07-19 01:15:42 +0900

    make binomial

---
+++ test/math/Binomial_Coefficient_Prime_Mod.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod"
+
+#include"../../template/template.hpp"
+#include"../../utility/modint.hpp"
+#include"../../math/binomial.hpp"
+
+int main(){
+    int t, m; in(t,m);
+    modint::set_mod(m);
+    while (t--){
+        int n, k; in(n,k);
+        out(binomial<modint>::C(n,k).val());
+    }
+}
