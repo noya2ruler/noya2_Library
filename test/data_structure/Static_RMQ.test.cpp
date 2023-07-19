7ee4586b85f6a62e70b48f57fef1312c946b3bee
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-07-19 13:45:51 +0900

    rename sparse_table

--- test/data_structure/Static_RMQ.test.cpp
+++ test/data_structure/Static_RMQ.test.cpp
@@ -2,7 +2,7 @@
 
 #include"../../template/template.hpp"
 
-#include"../../data_structure/sparce_table.hpp"
+#include"../../data_structure/sparse_table.hpp"
 
 int op(int a, int b){
     return min(a,b);
