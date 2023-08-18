bbb4bf2160c736384dfe94e7b20a8ca9e7f94498
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-08-18 12:11:45 +0900

    Update timer.hpp

--- misc/timer.hpp
+++ misc/timer.hpp
@@ -1,21 +1,26 @@
+#pragma once
+
+#include<chrono>
+#include<iostream>
+
 namespace noya2{
 
 struct Timer {
   private:
-    chrono::high_resolution_clock::time_point start, end;
+    std::chrono::high_resolution_clock::time_point start, end;
  
   public:
     Timer() { set(); }
-    void set() { start = chrono::high_resolution_clock::now(); }
+    void set() { start = std::chrono::high_resolution_clock::now(); }
     int time() {
-        end = chrono::high_resolution_clock::now();
-        return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        end = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     }
     double dtime(){
         return (double)(time()) / 1000;
     }
     bool before(double T) { return time() < (int)(T * 1000); }
-    void print() { cerr << "elapsed time: " << (double)time() / 1000 << " sec" << endl; }
+    void print() { std::cerr << "elapsed time: " << (double)time() / 1000 << " sec" << std::endl; }
 };
 
 } // namespace noya2
