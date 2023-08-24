a1990c2b33cfb87255f73f1155203895f6af3a57
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-08-24 20:23:56 +0900

    add Scanable, Printable

--- template/inout.hpp
+++ template/inout.hpp
@@ -1,47 +1,52 @@
 namespace noya2 {
 
-template <typename T> ostream &operator<<(ostream &os, const vector<T> &v);
-template <typename T> istream &operator>>(istream &is, vector<T> &v);
+template<typename T> concept Scanable  = requires (ifstream &is, T &a){ is >> a; };
+template<typename T> concept Printable = requires (ofstream &os, T &a){ os << a; };
+
+template <typename Scanable> istream &operator>>(istream &is, vector<Scanable> &v);
+template <typename Printable> ostream &operator<<(ostream &os, const vector<Printable> &v);
 
 template <typename T, typename U>
-ostream &operator<<(ostream &os, const pair<T, U> &p){
-    os << p.first << " " << p.second;
-    return os;
-}
-template <typename T, typename U>
+requires Scanable<T> && Scanable<U>
 istream &operator>>(istream &is, pair<T, U> &p){
     is >> p.first >> p.second;
     return is;
 }
+template <typename T, typename U>
+requires Printable<T> && Printable<U>
+ostream &operator<<(ostream &os, const pair<T, U> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
 
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v){
+template <typename Scanable>
+istream &operator>>(istream &is, vector<Scanable> &v){
+    for (auto &x : v) is >> x;
+    return is;
+}
+template <typename Printable>
+ostream &operator<<(ostream &os, const vector<Printable> &v){
     int s = (int)v.size();
     for (int i = 0; i < s; i++) os << (i ? " " : "") << v[i];
     return os;
 }
-template <typename T>
-istream &operator>>(istream &is, vector<T> &v){
-    for (auto &x : v) is >> x;
-    return is;
-}
 
 void in() {}
-template <typename T, class... U>
-void in(T &t, U &...u){
+template <typename Scanable, class... U>
+void in(Scanable &t, U &...u){
     cin >> t;
     in(u...);
 }
 
 void out() { cout << "\n"; }
-template <typename T, class... U, char sep = ' '>
-void out(const T &t, const U &...u){
+template <typename Printable, class... U, char sep = ' '>
+void out(const Printable &t, const U &...u){
     cout << t;
     if (sizeof...(u)) cout << sep;
     out(u...);
 }
-template<typename T>
-void out(const vector<vector<T>> &vv){
+template<typename Printable>
+void out(const vector<vector<Printable>> &vv){
     int s = (int)vv.size();
     for (int i = 0; i < s; i++) out(vv[i]);
 }
