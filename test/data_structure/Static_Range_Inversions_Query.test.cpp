1d10779557df38ab58d17eb20f5284358e6ba560
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-09-01 21:00:32 +0900

    Create Static_Range_Inversions_Query.test.cpp

---
+++ test/data_structure/Static_Range_Inversions_Query.test.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+
+#include"../../template/template.hpp"
+#include"../../data_structure/compress.hpp"
+#include"../../data_structure/binary_indexed_tree.hpp"
+#include"../../misc/mo_algorithm.hpp"
+
+int main(){
+    int n, q; in(n,q);
+    vector<int> a(n); in(a);
+    compress<int> cp(a);
+    rep(i,n) a[i] = cp.id(a[i]);
+    Mo mo(n,q);
+    rep(i,q){
+        int l, r; in(l,r);
+        mo.insert(l,r);
+    }
+    BIT_Plus<ll> bit(n);
+    ll cur = 0;
+    auto addl = [&](int i){
+        cur += bit.prod(0,a[i]);
+        bit.add(a[i],1);
+    };
+    auto addr = [&](int i){
+        cur += bit.prod(a[i]+1,n);
+        bit.add(a[i],1);
+    };
+    auto dell = [&](int i){
+        cur -= bit.prod(0,a[i]);
+        bit.add(a[i],-1);
+    };
+    auto delr = [&](int i){
+        cur -= bit.prod(a[i]+1,n);
+        bit.add(a[i],-1);
+    };
+    vector<ll> ans(q,0);
+    auto ask = [&](int i){
+        ans[i] = cur;
+    };
+    mo.run(addl,addr,dell,delr,ask);
+    rep(i,q) out(ans[i]);
+}
