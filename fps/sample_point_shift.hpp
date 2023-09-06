ffaefccd92164e64ef914024c848d711248df867
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-09-06 22:54:00 +0900

    Create sample_point_shift.hpp

---
+++ fps/sample_point_shift.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include"../template/template.hpp"
+#include"../fps/fps_ntt.hpp"
+
+namespace noya2{
+
+template<typename mint>
+FPS_ntt<mint> sample_point_shift(FPS_ntt<mint> y, typename FPS_ntt<mint>::value_type t, int m){
+    using fps = FPS_ntt<mint>;
+    ll T = t.val();
+    int k = (int)(y.size()) - 1;
+    if (T <= k){
+        fps ret(m);
+        int ptr = 0;
+        for (ll i = T; i <= k && ptr < m; i++){
+            ret[ptr++] = y[i];
+        }
+        if (k+1 < T+m){
+            auto suf = sample_point_shift(y,k+1,m-ptr);
+            for (int i = k+1; i < T+m; i++){
+                ret[ptr++] = suf[i-(k+1)];
+            }
+        }
+        return ret;
+    }
+    if (T+m > mint::mod()){
+        auto pref = sample_point_shift(y,T,mint::mod()-T);
+        auto suf  = sample_point_shift(y,0,m-(int)(pref.size()));
+        copy(suf.begin(),suf.end(),back_inserter(pref));
+        return pref;
+    }
+    binomial<mint> bnm;
+    fps d(k+1);
+    for (int i = 0; i <= k; i++){
+        d[i] = bnm.ifact(i) * bnm.ifact(k-i) * y[i];
+        if ((k-i)&1) d[i] = -d[i];
+    }
+    vector<mint> fact(m+k+1); fact[0] = 1;
+    for (int i = 0; i < m+k; i++) fact[i+1] = fact[i] * (T-k+i);
+    fps h(m+k); h[m+k-1] = fact[m+k].inv();
+    for (int i = m+k-1; i >= 1; i--) h[i-1] = h[i] * (T-k+i);
+    for (int i = 0; i < m+k; i++) h[i] *= fact[i];
+    auto dh = d * h;
+    fps ret(m);
+    mint cur = T;
+    for (int i = 1; i <= k; i++) cur *= T-i;
+    for (int i = 0; i < m; i++){
+        ret[i] = cur * dh[k+i];
+        cur *= T+i+1;
+        cur *= h[i];
+    }
+    return ret;
+}
+
+} // namespace noya2
