cc054ef595b16d6a3494662208287fd9a0ef6872
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-09-09 12:30:18 +0900

    Create vector2d.hpp

---
+++ data_structure/vector2d.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include<vector>
+#include<ranges>
+
+namespace noya2{
+
+template<class E>
+struct vector2d {
+    vector2d (int n_ = 0, int m_ = -1) : n(n_), m(m_) {
+        if (m == -1) m = n-1;
+        es.resize(m);
+        start.resize(m,0);
+    }
+    void add(int from, E e){
+        static int id = 0;
+        es[id] = e, start[id] = from;
+        if (++id == m) build();
+    }
+    const auto operator[](int idx){ return std::ranges::subrange(es.begin()+start[idx],es.begin()+start[idx+1]); }
+  private:
+    void build(){
+        std::vector<E> nes(m);
+        std::vector<int> nstart(n+1,0);
+        for (int i = 0; i < m; i++) nstart[start[i]+1]++;
+        for (int i = 0; i < n; i++) nstart[i+1] += nstart[i];
+        auto geta = nstart;
+        for (int i = 0; i < m; i++) nes[geta[start[i]]++] = es[i];
+        std::swap(nes,es); std::swap(nstart,start);
+    }
+    int n, m;
+    std::vector<E> es;
+    std::vector<int> start;
+};
+
+} // namespace noya2
