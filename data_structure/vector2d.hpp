4261ea8c8d2f4efa68810c8c4d1abcaf0a436617
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2023-09-22 14:26:46 +0900

    element id should not be static

--- data_structure/vector2d.hpp
+++ data_structure/vector2d.hpp
@@ -7,16 +7,14 @@ namespace noya2{
 
 template<class E>
 struct vector2d {
-    vector2d (int n_ = 0, int m_ = -1) : n(n_), m(m_) {
-        if (m == -1) m = n-1;
+    vector2d (int n_ = 0, int m_ = 0) : n(n_), m(m_), inner_element_id(0) {
         es.resize(m);
         start.resize(m,0);
         if (m == 0) build();
     }
     void add(int from, E e){
-        static int id = 0;
-        es[id] = e, start[id] = from;
-        if (++id == m) build();
+        es[inner_element_id] = e, start[inner_element_id] = from;
+        if (++inner_element_id == m) build();
     }
     const auto operator[](int idx){ return std::ranges::subrange(es.begin()+start[idx],es.begin()+start[idx+1]); }
   private:
@@ -29,7 +27,7 @@ struct vector2d {
         for (int i = 0; i < m; i++) nes[geta[start[i]]++] = es[i];
         std::swap(nes,es); std::swap(nstart,start);
     }
-    int n, m;
+    int n, m, inner_element_id;
     std::vector<E> es;
     std::vector<int> start;
 };
