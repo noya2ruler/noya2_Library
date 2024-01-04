b9ca93b993ac531b93459db14a6821dae5662a5b
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-01-04 13:43:08 +0900

    Create Shortest_Path2.test.cpp

---
+++ test/graph/Shortest_Path2.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/shortest_path"
+
+#include"../../template/template.hpp"
+#include"../../graph/graph_query.hpp"
+
+int main(){
+    int n, m, s, t; in(n,m,s,t);
+    Graph<ll> g(n,m);
+    rep(i,m){
+        int u, v; in(u,v);
+        ll c; in(c);
+        g.add_edge(u,v,c);
+    }
+    auto dist = g.dijkstra(s);
+    if (dist[t] == g.dist_inf){
+        out(-1);
+        return 0;
+    }
+    auto ans = g.reconstruct(s,t,dist);
+    out(dist[t],ans.size()-1);
+    rep(i,ans.size()-1) out(ans[i],ans[i+1]);
+}
