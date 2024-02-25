95308d21bdbe3ff62c7006e0e132f88a8a12115a
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-02-25 21:32:37 +0900

    Create simple_tree.hpp

---
+++ tree/simple_tree.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include"../template/template.hpp"
+#include"../data_structure/csr.hpp"
+
+namespace noya2 {
+
+struct simple_tree {
+    internal::csr<int> g;
+    simple_tree () {}
+    simple_tree (int _n) : g(_n, (_n - 1)*2) {}
+    void add_edge(int u, int v){
+        g.add(u, v);
+        int id = g.add(v, u);
+        if (id + 1 == (g.n - 1)*2) g.build();
+    }
+    void input(int indexed = 1){
+        for (int i = 0; i < g.n - 1; i++){
+            int u, v; cin >> u >> v;
+            u -= indexed, v -= indexed;
+            add_edge(u, v);
+        }
+    }
+    void input_parents(int indexed = 1){
+        for (int i = 0; i < g.n - 1; i++){
+            int v; cin >> v;
+            v -= indexed;
+            add_edge(i + 1, v);
+        }
+    }
+    const auto operator[](int v) const {
+        return g[v];
+    }
+    auto operator[](int v){
+        return g[v];
+    }
+};
+
+} // namespace noya2
