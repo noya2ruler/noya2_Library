de8b78ac4c8a8d781ab58257ebb1224005c86721
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-06-19 21:22:15 +0900

    Update range_parallel_dsu.hpp

--- data_structure/range_parallel_dsu.hpp
+++ data_structure/range_parallel_dsu.hpp
@@ -4,42 +4,44 @@
 
 namespace noya2 {
 
-struct range_parallel_dsu {
+struct range_parallel_dsu : dsu {
+    using dsu::operator=;
     int n;
     std::vector<dsu> ds;
-    range_parallel_dsu (int _n) : n(_n) {
+    range_parallel_dsu (int _n0) : n(_n0) {
         int msb = 31 - countl_zero((unsigned int)(n));
-        ds.resize(msb+1,dsu(n));
+        ds.resize(msb, dsu(n));
+        (*this) = dsu(n);
     }
     // for k = 0, 1, ... ,d-1
     // merge(i+k,j+k)
-    void merge(int i, int j, int d){
+    std::vector<std::tuple<int,int,int>> merge_range(int i, int j, int d){
         assert(i + d <= n);
         assert(j + d <= n);
-        if (d == 0) return ;
-        int k2 = std::bit_floor((unsigned int)(d));
-        int msb = 31 - countl_zero((unsigned int)(k2));
-        inner_merge(i, j, msb);
-        inner_merge(i + d - k2, j + d - k2, msb);
-    }
-    int same(int i, int j){
-        return ds[0].same(i, j);
-    }
-    int size(int i){
-        return ds[0].size(i);
-    }
-    int leader(int i){
-        return ds[0].leader(i);
+        if (d == 0) return {};
+        int msb = std::bit_width((unsigned int)(d)) - 1;
+        std::vector<std::tuple<int,int,int>> connected;
+        inner_merge(i, j, msb, connected);
+        inner_merge(i + d - (1<<msb), j + d - (1<<msb), msb, connected);
+        return connected;
     }
   private:
     // d = 2^k
-    void inner_merge(int i, int j, int k){
+    void inner_merge(int i, int j, int k, std::vector<std::tuple<int,int,int>> &connected){
         // k >= 0
-        if (ds[k].same(i, j)) return ;
-        ds[k].merge(i,j);
-        if (k >= 1){
-            inner_merge(i, j, k-1);
-            inner_merge(i + (1<<(k-1)), j + (1<<(k-1)), k-1);
+        if (k == 0){
+            i = this->leader(i);
+            j = this->leader(j);
+            if (i == j) return ;
+            int l = this->merge(i, j);
+            connected.emplace_back(i, j, l);
+        }
+        else {
+            k--;
+            if (ds[k].same(i, j)) return ;
+            ds[k].merge(i, j);
+            inner_merge(i, j, k, connected);
+            inner_merge(i + (1<<k), j + (1<<k), k, connected);
         }
     }
 };
