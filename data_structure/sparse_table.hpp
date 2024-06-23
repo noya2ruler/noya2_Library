b2eaa26fd35e94319afbed5dc9651feb89c3e6f3
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-06-23 15:42:36 +0900

    not use template

--- data_structure/sparse_table.hpp
+++ data_structure/sparse_table.hpp
@@ -1,28 +1,27 @@
 #pragma once
 
-#include"../template/template.hpp"
+#include <vector>
+#include <cassert>
 
 namespace noya2{
 
 template<class S, S (*op)(S, S)>
 struct SparseTable{
-    vector<vector<S>> table;
+    std::vector<std::vector<S>> table;
     SparseTable () {}
-    SparseTable (const vector<S> &vec){
+    SparseTable (const std::vector<S> &vec){
         int n = vec.size(), n2 = 0;
         while ((1<<n2) < n) n2++;
         table.resize(n2+1);
-        for (int i = 0; i <= n2; i++){
+        table[0] = vec;
+        for (int i = 1; i <= n2; i++){
             table[i].resize(n);
-            if (i == 0){
-                for (int j = 0; j < n; j++){
-                    table[i][j] = vec[j];
+            for (int j = 0; j < n; j++){
+                if (j + (1 << (i-1)) < n){
+                    table[i][j] = op(table[i-1][j], table[i-1][j + (1 << (i-1))]);
                 }
-            }
-            else {
-                for (int j = 0; j < n; j++){
-                    if (j + (1 << (i-1)) < n) table[i][j] = op(table[i-1][j],table[i-1][j + (1 << (i-1))]);
-                    else table[i][j] = table[i-1][j];
+                else {
+                    table[i][j] = table[i-1][j];
                 }
             }
         }
