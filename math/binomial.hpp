ca5aa36ea086dfeba9a13e7d93d561240e012e45
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-07-03 11:54:46 +0900

    Update binomial.hpp

--- math/binomial.hpp
+++ math/binomial.hpp
@@ -28,15 +28,22 @@ struct binomial {
         return fact(n) * ifact(n-r);
     }
     inline mint operator()(int n, int r) { return C(n, r); }
-    template<class... Cnts> static mint M(const Cnts&... cnts){
+    template<class... Cnts>
+    static mint M(const Cnts&... cnts){
         return multinomial(0,1,cnts...);
+    }
+    static void initialize(int len = 2){
+        _fact.clear();
+        _ifact.clear();
+        extend(len);
     }
   private:
     static mint multinomial(const int& sum, const mint& div_prod){
         if (sum < 0) return 0;
         return fact(sum) * div_prod;
     }
-    template<class... Tail> static mint multinomial(const int& sum, const mint& div_prod, const int& n1, const Tail&... tail){
+    template<class... Tail>
+    static mint multinomial(const int& sum, const mint& div_prod, const int& n1, const Tail&... tail){
         if (n1 < 0) return 0;
         return multinomial(sum+n1,div_prod*ifact(n1),tail...);
     }
@@ -53,11 +60,6 @@ struct binomial {
         for (int i = siz; i <= len; i++) _fact[i] = _fact[i-1] * i;
         _ifact[len] = _fact[len].inv();
         for (int i = len; i > siz; i--) _ifact[i-1] = _ifact[i] * i;
-    }
-    static void initialize(int len = 2){
-        _fact.clear();
-        _ifact.clear();
-        extend(len);
     }
 };
 
