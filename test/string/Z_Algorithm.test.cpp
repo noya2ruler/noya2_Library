67be58cf07be00c3a4c46afc45fa827999bd9372
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2024-12-04 14:32:21 +0900

    Create Z_Algorithm.test.cpp

---
+++ test/string/Z_Algorithm.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+
+#include"../../template/template.hpp"
+#include"../../string/z_algorithm.hpp"
+
+int main(){
+    string s; in(s);
+    out(z_algorithm(s));
+}
