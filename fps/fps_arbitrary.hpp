397600f90ec6da389496bf03e45b5276a0c205e6
Noya2 <107465724+noya2ruler@users.noreply.github.com>
2025-02-22 20:23:23 +0900

    Update fps_arbitrary.hpp

--- fps/fps_arbitrary.hpp
+++ fps/fps_arbitrary.hpp
@@ -6,14 +6,21 @@ namespace noya2{
 
 template<typename T>
 struct fps_arbitrary{
+    using value_type = T;
     static vector<T> multiply(const vector<T> &a, const vector<T> &b){
         if (a.empty() || b.empty()) return {};
         vector<T> res((int)(a.size() + b.size())-1,T(0));
         rep(i,a.size()) rep(j,b.size()) res[i+j] += a[i] * b[j];
         return res;
     }
+    static vector<T> inv(const vector<T> &a, int d = -1){
+        abort();
+    }
+    static vector<T> integral(const vector<T> &a){
+        abort();
+    }
 };
-template<typename T> using fps = FormalPowerSeries<T,fps_arbitrary<T>>;
+template<typename T> using fps = FormalPowerSeries<fps_arbitrary<T>>;
 
 } // namespace noya2
 
